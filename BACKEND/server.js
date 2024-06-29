23cd495f0a0a5b15cc5cb17cf5400b59bd95d0a5
dasun dhammasara <dasundhammasara9@gmail.com>
2024-06-29 11:31:05 +0530

    test commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT || 8070;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
